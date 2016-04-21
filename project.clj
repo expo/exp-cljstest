cf706d55d391d1ab1192c04116c4e5bcdf3a3900
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2016-04-20 21:03:37 -0700

    get cider awesomeness

--- project.clj
+++ project.clj
@@ -7,15 +7,17 @@
                  [org.clojure/clojurescript "1.7.170"]
                  [reagent "0.5.1" :exclusions [cljsjs/react]]
                  [re-frame "0.6.0"]
-                 [prismatic/schema "1.0.4"]]
+                 [prismatic/schema "1.0.4"]
+                 [com.cemerick/piggieback "0.2.1"]]
   :plugins [[lein-cljsbuild "1.1.1"]
-            [lein-figwheel "0.5.0-6"]]
+            [lein-figwheel "0.5.0-6"]
+            [cider/cider-nrepl "0.12.0"]
+            [refactor-nrepl "2.0.0-SNAPSHOT"]]
   :clean-targets ["target/" "index.js"]
   :aliases {"prod-build" ^{:doc "Recompile code with prod profile."}
             ["do" "clean"
              ["with-profile" "prod" "cljsbuild" "once" "main"]]}
-  :profiles {:dev {:dependencies [[figwheel-sidecar "0.5.0-6"]
-                                  [com.cemerick/piggieback "0.2.1"]]
+  :profiles {:dev {:dependencies [[figwheel-sidecar "0.5.0-6"]]
                    :source-paths ["src" "env/dev"]
                    :cljsbuild    {:builds {:main {:source-paths ["src" "env/dev"]
                                                   :figwheel     true
@@ -29,4 +31,8 @@
                                                                :main          "env.main"
                                                                :output-dir    "target"
                                                                :optimizations :simple}}}}}}
-  :figwheel { :nrepl-port 7888 })
+  :figwheel {:nrepl-port 7888
+
+             :nrepl-middleware ["cider.nrepl/cider-middleware"
+                                "refactor-nrepl.middleware/wrap-refactor"
+                                "cemerick.piggieback/wrap-cljs-repl"]})
