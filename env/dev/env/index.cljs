9e15ef731ae61f938dd8a506c62a128949894801
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2016-04-22 21:02:41 -0700

    use nicer syntax for js objects

--- env/dev/env/index.cljs
+++ env/dev/env/index.cljs
@@ -4,8 +4,8 @@
 (set! js/window.goog js/undefined)
 
 (-> (js/require "figwheel-bridge")
-    (.withModules (js-obj "dev-env" (js/require "./dev-env.js")
-                          "react-native" (js/require "react-native")
-                          "react" (js/require "react")))
+    (.withModules #js{:dev-env (js/require "./dev-env.js")
+                      :react-native (js/require "react-native")
+                      :react (js/require "react")})
     (.start "main"))
 
